dd5ae82858fc16d6f80c56aa9f8de722309f9e37
ender <endernagshushman@gmail.com>
2024-06-11 21:04:37 +0200

    added docker support for app

---
+++ backend/Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.10.4-slim-buster
+LABEL maintainer="endernagashuhsman@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+RUN adduser \
+    --disabled-password \
+    --no-create-home \
+    django-user
+
+
+USER django-user
--- backend/usolapohvist/urls.py
+++ backend/usolapohvist/urls.py
@@ -14,15 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("usolapohvist_app.urls", namespace="usolapohvist_app"))
+    path("api/", include("usolapohvist_app.urls", namespace="usolapohvist_app")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
