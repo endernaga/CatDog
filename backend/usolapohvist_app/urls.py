0fb1c1fd1aef09c0c041a3a960cb200b0a1b5a51
ender <endernagshushman@gmail.com>
2024-07-01 11:35:48 +0300

    added notification bot for saving form

--- backend/usolapohvist_app/urls.py
+++ backend/usolapohvist_app/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from usolapohvist_app.views import CatViewSet, DogViewSet, guess_the_sex
+from usolapohvist_app.views import CatViewSet, DogViewSet, guess_the_sex, save_sos_form
 
 router = routers.DefaultRouter()
 router.register("cats", CatViewSet)
 router.register("dogs", DogViewSet)
 
-urlpatterns = [path("", include(router.urls)), path("heOrShe/", guess_the_sex)]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("heOrShe/", guess_the_sex),
+    path("sos/", save_sos_form),
+]
 
 
 app_name = "usolapohvist_app"
