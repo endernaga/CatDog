0a419aa52a125345e1031afaeffe320ebc7f3d68
ender <endernagshushman@gmail.com>
2024-07-01 17:24:57 +0300

    added endpoint to get all animals

--- backend/usolapohvist_app/urls.py
+++ backend/usolapohvist_app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from usolapohvist_app.views import CatViewSet, DogViewSet, guess_the_sex, save_sos_form
+from usolapohvist_app.views import (
+    CatViewSet,
+    DogViewSet,
+    guess_the_sex,
+    save_sos_form,
+    Animals,
+)
 
 router = routers.DefaultRouter()
 router.register("cats", CatViewSet)
 router.register("dogs", DogViewSet)
+router.register("animals", Animals, basename="animals")
 
 urlpatterns = [
     path("", include(router.urls)),
