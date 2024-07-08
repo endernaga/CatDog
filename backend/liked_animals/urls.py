a5bf82a0ee431dbdb4b9f5b5e9e401bed878550d
ender <endernagshushman@gmail.com>
2024-07-08 21:26:14 +0300

    fixed animals endpoint photo argument bug

--- backend/liked_animals/urls.py
+++ backend/liked_animals/urls.py
@@ -1,13 +1,4 @@
 from django.urls import path
-
-<<<<<<< HEAD
-from liked_animals.views import liked_animals, get_liked_cats, get_liked_dogs
-
-urlpatterns = [
-    path("liked_animals/", liked_animals),
-    path("liked_cats/", get_liked_cats),
-    path("liked_dogs/", get_liked_dogs),
-=======
 from liked_animals.views import (
     liked_animals,
     liked_cats,
@@ -22,7 +13,6 @@ urlpatterns = [
     path("liked_dogs/", liked_dogs),
     path("liked_dogs/<int:pk>/", delete_liked_dog),
     path("liked_cats/<int:pk>/", delete_liked_cat),
->>>>>>> origin/developers
 ]
 
 app_name = "liked_animals"
